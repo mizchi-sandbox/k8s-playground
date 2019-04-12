732711d5010ab25545f53efb229f4943f6923a18
mizchi <miz404@gmail.com>
2019-04-12 17:36:42 +0900

    Update

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ const http = require("http");
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World\n");
+    res.end("Hello World2\n");
   })
   .listen(3000);
